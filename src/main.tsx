6173968c9b8542c5dba16b9ca8cf8a2454771fd2
skyunlee98 <skyunlee@naver.com>
2025-07-18 11:25:01 +0900

    main 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// 메인 컴포넌트
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
